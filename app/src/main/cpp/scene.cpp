d28b78c665a1d66a8df46aa54a82427f1ff54a51
renzhenming <1565766177@qq.com>
2019-04-20 09:34:28 +0800

    用封装好的shader绘制三角形

--- app/src/main/cpp/scene.cpp
+++ app/src/main/cpp/scene.cpp
@@ -2,51 +2,34 @@
 #include "utils.h"
 #include "ground.h"
 
-GLuint vbo, ebo;
-GLuint program;
-GLint positionLocation, modelMatrixLocation, viewMatrixLocation, projectMatrixLocation, colorLocation;
 glm::mat4 modelMatrix, viewMatrix, projectMatrix;
-GLint texcoordLocation, textureLocation;
+GLint texcoordLocation;
 GLuint texture;
 Ground ground;
+Shader *shader;
+VertexBuffer *vertexBuffer = NULL;
 
 void Init() {
     glClearColor(0.1f, 0.4f, 0.6f, 1.0f);
-//    float data[] = {
-//            //增加颜色属性
-//            -0.2f, -0.2f, -0.6f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-//            0.2f, -0.2f, -0.6f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f,
-//            0.0f, 0.2f, -0.6f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.5f, 1.0f
-//    };
-//
-//    //ElementBuffer控制vbo数据的绘制顺序
-//    unsigned short indexes[] = {0, 1, 2};
-//
-//    vbo = CreateBufferObject(GL_ARRAY_BUFFER, sizeof(float) * 30, GL_STATIC_DRAW, data);
-//    ebo = CreateBufferObject(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned short) * 3, GL_STATIC_DRAW,
-//                             indexes);
-//
-//    int filesize = 0;
-//    unsigned char *vShaderCode = LoadFileContent("Res/test.vs", filesize);
-//    GLuint vShader = CompileShader(GL_VERTEX_SHADER, (const char *) vShaderCode);
-//    delete vShaderCode;
-//    unsigned char *fShaderCode = LoadFileContent("Res/test.fs", filesize);
-//    GLuint fShader = CompileShader(GL_FRAGMENT_SHADER, (const char *) fShaderCode);
-//    delete fShaderCode;
-//    program = CreateProgram(vShader, fShader);
-//    glDeleteShader(vShader);
-//    glDeleteShader(fShader);
-//
-//    positionLocation = glGetAttribLocation(program, "position");
-//    colorLocation = glGetAttribLocation(program, "color");
-//    texcoordLocation = glGetAttribLocation(program, "texcoord");
-//
-//    modelMatrixLocation = glGetUniformLocation(program, "ModelMatrix");
-//    viewMatrixLocation = glGetUniformLocation(program, "ViewMatrix");
-//    projectMatrixLocation = glGetUniformLocation(program, "ProjectionMatrix");
-//
-//    textureLocation = glGetUniformLocation(program, "U_Texture");
-//    texture = CreateTexture2DFromBMP("Res/test2.bmp");
+    vertexBuffer = new VertexBuffer;
+    vertexBuffer->SetSize(3);
+    vertexBuffer->SetPosition(0,-0.2f,-0.2f,-0.6f,1.0f);
+    vertexBuffer->SetTexcoord(0,0.0f,0.0f);
+    vertexBuffer->SetColor(0,1.0f,0.0f,0.0f,0.0f);//红色
+
+    vertexBuffer->SetPosition(1,0.2f,-0.2f,-0.6f,1.0f);
+    vertexBuffer->SetTexcoord(1,1.0f,0.0f);
+    vertexBuffer->SetColor(1,0.0f,2.0f,0.0f,0.0f);//绿色
+
+    vertexBuffer->SetPosition(2,0.0f,0.2f,-0.6f,1.0f);
+    vertexBuffer->SetTexcoord(2,0.5f,1.0f);
+    vertexBuffer->SetColor(2,0.0f,0.0f,1.0f,1.0f);//蓝色
+
+    shader = new Shader;
+    shader->Init("Res/test.vs","Res/test.fs");
+    texcoordLocation=glGetUniformLocation(shader->program,"U_Texture");
+    texture=CreateTexture2DFromBMP("Res/test2.bmp");
+
     ground.Init();
 }
 
@@ -58,4 +41,12 @@ void SetViewPortSize(float width, float height) {
 void Draw() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     ground.Draw(viewMatrix, projectMatrix);
+
+    vertexBuffer->Bind();
+
+    shader->Bind(glm::value_ptr(modelMatrix),glm::value_ptr(viewMatrix),glm::value_ptr(projectMatrix));
+    glBindTexture(GL_TEXTURE_2D,texture);
+    glUniform1i(GL_TEXTURE_2D,0);
+    glDrawArrays(GL_TRIANGLES,0,3);
+    vertexBuffer->UnBind();
 }
